d0db6caa2429309b36c4d755b04e08a1f83c5b38
Meriemchm <chamimeriem2302@gmail.com>
2025-08-25 11:21:04 +0100

    fetch api single product

---
+++ actions/get-product.tsx
@@ -0,0 +1,10 @@
+import {Product} from '@/types';
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
+export const getProduct = async (id: string): Promise<Product[]> => {
+  const res = await fetch(`${URL}/${id}`);
+  if (!res.ok) {
+    throw new Error('Failed to fetch Product');
+  }
+  return res.json();
+};  
