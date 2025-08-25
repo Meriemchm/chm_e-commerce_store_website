a5a7acfafaac308caa531b6250f23fe7770e6110
Meriemchm <chamimeriem2302@gmail.com>
2025-08-25 16:37:40 +0100

    create get colors

---
+++ actions/get-colors.tsx
@@ -0,0 +1,10 @@
+import {Color} from '@/types';
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`;
+export const getColors = async (): Promise<Color[]> => {
+  const res = await fetch(URL);
+  if (!res.ok) {
+    throw new Error('Failed to fetch colors');
+  }
+  return res.json();
+};  
