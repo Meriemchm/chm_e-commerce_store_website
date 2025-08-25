0fdae4cedbb0fc739dddc4c8b86c290af70697fa
Meriemchm <chamimeriem2302@gmail.com>
2025-08-25 17:25:03 +0100

    create the modal provider and preview

---
+++ components/preview-modal.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import usePreviewModal from "@/hooks/use-preview-modal";
+import Modal from "./ui/modal";
+import { Gallery } from "./gallery";
+import { Info } from "./info";
+
+const PreviewModal = () => {
+  const PreviewModal = usePreviewModal();
+  const product = usePreviewModal((state) => state.data);
+
+  if (!product) {
+    return null;
+  }
+  return (
+    <Modal open={PreviewModal.isOpen} onClose={PreviewModal.onClose}>
+      <div className="grid w-full grid-cols-1 item-start gap-x-6 gap-y-8 sm:grid-col-12 lg:gap-x-8">
+        <div className="">
+          <Gallery images={product.images} />
+        </div>
+        <div className="sm:col-span-8  lg:col-span-7">
+          <Info data={product} />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default PreviewModal;
