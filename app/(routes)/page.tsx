9ef50dd4471e0def1064dd64da3f1371ee4bda8e
Meriemchm <chamimeriem2302@gmail.com>
2025-08-25 11:24:32 +0100

    edit pricipal page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,26 @@
-export default function HomePage() {
+import { getBillboard } from "@/actions/get-billboard";
+import { getProducts } from "@/actions/get-products";
+import { Billboard } from "@/components/billboard";
+import { ProductList } from "@/components/product/product-list";
+import { Container } from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("99ba29be-a21c-42b6-afc0-dd3e2ade6fef");
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <Container>
+      <div className="">
+        <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 py-5">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
