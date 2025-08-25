492c5a7930f756a95a898e0810c30a152e1b4902
Meriemchm <chamimeriem2302@gmail.com>
2025-08-25 16:40:59 +0100

    fix some rendering issues

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,7 @@
 import { getProduct } from "@/actions/get-product";
 import { getProducts } from "@/actions/get-products";
+import { Gallery } from "@/components/gallery/index";
+import { Info } from "@/components/info";
 import { ProductList } from "@/components/product/product-list";
 import { Container } from "@/components/ui/container";
 
@@ -18,15 +20,15 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
     })
   ).filter((p) => p.id !== product?.id);
 
-  console.log(suggestedProducts);
-
   return (
     <div className="bg-white rounded-lg shadow p-6">
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8  ">
-            <div>Gallery</div>
-            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">Info</div>
+            <Gallery images={product.images} />
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <Info data={product} />
+            </div>
           </div>
           <hr className="my-10" />
           <ProductList title="Related Products" items={suggestedProducts} />
