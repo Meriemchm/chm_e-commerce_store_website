f9feed63c668bb442e40f56766d3591b2f27cd86
Meriemchm <chamimeriem2302@gmail.com>
2025-08-25 11:21:26 +0100

    fetch billboard

---
+++ actions/get-billboard.tsx
@@ -0,0 +1,10 @@
+import {Billboard} from '@/types';
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
+export const getBillboard = async (id: string): Promise<Billboard[]> => {
+  const res = await fetch(`${URL}/${id}`);
+  if (!res.ok) {
+    throw new Error('Failed to fetch billboard');
+  }
+  return res.json();
+};  
