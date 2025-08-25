8c3cbd6f9eb98e0ae5ed1e429a1d2a88a4e42662
Meriemchm <chamimeriem2302@gmail.com>
2025-08-25 11:23:34 +0100

    create type to securise my code

--- types.ts
+++ types.ts
@@ -1,10 +1,37 @@
 export interface Billboard {
   id: string;
-  name: string;
+  label: string;
   imageUrl: string;
 }
 export interface Category {
   id: string;
   name: string;
   Billboard: Billboard;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  category: Category;
+  price: string;
+  images: Image[];
+  isFeatured: boolean;
+  size: Size;
+  color: Color;
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Color {
+  id: string;
+  name: string;
+  value: string;
 }
