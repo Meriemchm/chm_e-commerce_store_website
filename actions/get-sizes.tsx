c6bafec1ead557ae47e47243c25d3466e9ed5949
Meriemchm <chamimeriem2302@gmail.com>
2025-08-25 16:38:38 +0100

    get sizes

---
+++ actions/get-sizes.tsx
@@ -0,0 +1,10 @@
+import {Size} from '@/types';
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes`;
+export const getSizes = async (): Promise<Size[]> => {
+  const res = await fetch(URL);
+  if (!res.ok) {
+    throw new Error('Failed to fetch sizes');
+  }
+  return res.json();
+};  
